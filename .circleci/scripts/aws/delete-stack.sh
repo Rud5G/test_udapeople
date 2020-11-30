b142bd386d60b09d2255c64bfb15968ddb6d9e8c
Rudger Gravestein <r.gravestein@triple-networks.com>
2020-11-30 18:38:35 +0100

    Test destroy-environment

--- .circleci/scripts/aws/delete-stack.sh
+++ .circleci/scripts/aws/delete-stack.sh
@@ -2,9 +2,9 @@
 
 #set -x
 
-set -ue
+#set -ue
 
-delete_stack_name="$1"
+delete_stack_name="${1:-}"
 
 # shellcheck source=./aws-common-cli.sh
 . "$(dirname "$0")/aws-common-cli.sh"
@@ -17,4 +17,14 @@ delete_stack_name="$1"
 
 #aws_list_json_not_deleted_stacks
 
-aws_delete_stack "$delete_stack_name"
+if test -n "$DS_STACKNAME";
+then
+    aws_delete_stack "$DS_STACKNAME"
+else
+    aws_delete_stack "$delete_stack_name"
+fi
+
+
+
+
+
